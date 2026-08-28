97e91add6caa03015d80d8ce761657987e5aacfb
FariqDaffaD <fariqdaffa08@gmail.com>
2026-08-28 15:33:33 +0700

    Update template PDF surat tugas survey dan instalasi dengan kop baru PT Connecti Jelajah Priangan

--- resources/views/pdf/surat-tugas-instalasi.blade.php
+++ resources/views/pdf/surat-tugas-instalasi.blade.php
@@ -5,15 +5,31 @@
     <title>Surat Tugas Instalasi - {{ $customer->nomor_internet ?? '' }}</title>
     <style>
         @page {
-            margin: 45px 55px 45px 55px;
+            margin: 0px;
             size: a4 portrait;
         }
         body {
+            margin: 0px;
+            padding: 0px;
             font-family: Arial, Helvetica, sans-serif;
             font-size: 13px;
             line-height: 1.6;
             color: #000000;
             background-color: #ffffff;
+        }
+        .bg-kop {
+            position: fixed;
+            top: 0px;
+            left: 0px;
+            width: 100%;
+            height: 100%;
+            z-index: -1000;
+        }
+        .content-container {
+            padding-top: 135px;
+            padding-left: 60px;
+            padding-right: 60px;
+            padding-bottom: 50px;
         }
         .text-center {
             text-align: center;
@@ -27,7 +43,7 @@
         .nomor-surat {
             font-size: 13px;
             font-weight: bold;
-            margin-bottom: 35px;
+            margin-bottom: 30px;
         }
         .content-p {
             text-align: justify;
@@ -87,70 +103,81 @@
 </head>
 <body>
 
-    <div class="text-center">
-        <div class="title">SURAT TUGAS INSTALASI</div>
-        <div class="nomor-surat">Nomor : &nbsp;&nbsp;&nbsp;&nbsp;/CJP-NOC/{{ $instalasiMonth ?? date('m') }}/{{ $instalasiYear ?? date('Y') }}</div>
+    @php
+        $bgPath = public_path('img/kop-surat.png');
+        $bgBase64 = file_exists($bgPath) ? 'data:image/png;base64,' . base64_encode(file_get_contents($bgPath)) : null;
+    @endphp
+
+    @if($bgBase64)
+        <img src="{{ $bgBase64 }}" class="bg-kop" alt="Kop Surat">
+    @endif
+
+    <div class="content-container">
+        <div class="text-center">
+            <div class="title">SURAT TUGAS INSTALASI</div>
+            <div class="nomor-surat">Nomor : &nbsp;&nbsp;&nbsp;&nbsp;/CJP-NOC/{{ $instalasiMonth ?? date('m') }}/{{ $instalasiYear ?? date('Y') }}</div>
+        </div>
+
+        <div class="content-p">
+            Dengan surat ini pada tanggal, <strong>{{ $instalasiDateFormatted }}</strong> PT Connecti Jelajah Priangan menugaskan tim teknisi, <strong>{{ strtoupper($instalasiTeam) }}</strong>. Untuk melakukan pemasangan jaringan internet ke pada pelanggan baru dengan rincian sebagai berikut :
+        </div>
+
+        <table class="table-data">
+            <tr>
+                <td class="label-col">Nama Pelanggan</td>
+                <td class="colon-col">:</td>
+                <td class="val-col">{{ strtoupper($customerName) }}</td>
+            </tr>
+            <tr>
+                <td class="label-col">Alamat</td>
+                <td class="colon-col">:</td>
+                <td class="val-col">{{ strtoupper($installationAddress) }}</td>
+            </tr>
+            <tr>
+                <td class="label-col">Nomor Telepone</td>
+                <td class="colon-col">:</td>
+                <td class="val-col">{{ $phoneNumber ?: '-' }}</td>
+            </tr>
+            <tr>
+                <td class="label-col">Layanan</td>
+                <td class="colon-col">:</td>
+                <td class="val-col">{{ $serviceName }}</td>
+            </tr>
+            <tr>
+                <td class="label-col">Waktu Pengerjaan</td>
+                <td class="colon-col">:</td>
+                <td class="val-col">{{ $workingTime ?: '-' }}</td>
+            </tr>
+            <tr>
+                <td class="label-col">Detail Pekerjaan</td>
+                <td class="colon-col">:</td>
+                <td class="val-col">{{ $jobDetails ?: '-' }}</td>
+            </tr>
+        </table>
+
+        <div class="closing-p">
+            Demikian surat tugas ini dibuat dan dapat dipertanggung jawabkan dan dapat digunakan sebagaimana mestinya, terimakasih .
+        </div>
+
+        <div class="regards">
+            Hormat Kami PT Connecti Jelajah Priangan, Bandung.
+        </div>
+
+        <table class="table-signature">
+            <tr>
+                <td>Penanggung Jawab</td>
+                <td>Pelanggan</td>
+            </tr>
+            <tr>
+                <td class="space-sign"></td>
+                <td class="space-sign"></td>
+            </tr>
+            <tr>
+                <td class="sign-name">{{ strtoupper($personInCharge ?: 'IPIN ARIPIN') }}</td>
+                <td class="sign-name">{{ strtoupper($customerName) }}</td>
+            </tr>
+        </table>
     </div>
-
-    <div class="content-p">
-        Dengan surat ini pada tanggal, <strong>{{ $instalasiDateFormatted }}</strong> PT Media Solusi Network menugaskan tim teknisi, <strong>{{ strtoupper($instalasiTeam) }}</strong>. Untuk melakukan pemasangan jaringan internet ke pada pelanggan baru dengan rincian sebagai berikut :
-    </div>
-
-    <table class="table-data">
-        <tr>
-            <td class="label-col">Nama Pelanggan</td>
-            <td class="colon-col">:</td>
-            <td class="val-col">{{ strtoupper($customerName) }}</td>
-        </tr>
-        <tr>
-            <td class="label-col">Alamat</td>
-            <td class="colon-col">:</td>
-            <td class="val-col">{{ strtoupper($installationAddress) }}</td>
-        </tr>
-        <tr>
-            <td class="label-col">Nomor Telepone</td>
-            <td class="colon-col">:</td>
-            <td class="val-col">{{ $phoneNumber ?: '-' }}</td>
-        </tr>
-        <tr>
-            <td class="label-col">Layanan</td>
-            <td class="colon-col">:</td>
-            <td class="val-col">{{ $serviceName }}</td>
-        </tr>
-        <tr>
-            <td class="label-col">Waktu Pengerjaan</td>
-            <td class="colon-col">:</td>
-            <td class="val-col">{{ $workingTime ?: '-' }}</td>
-        </tr>
-        <tr>
-            <td class="label-col">Detail Pekerjaan</td>
-            <td class="colon-col">:</td>
-            <td class="val-col">{{ $jobDetails ?: '-' }}</td>
-        </tr>
-    </table>
-
-    <div class="closing-p">
-        Demikian surat tugas ini dibuat dan dapat dipertanggung jawabkan dan dapat digunakan sebagaimana mestinya, terimakasih .
-    </div>
-
-    <div class="regards">
-        Hormat Kami PT Media Solusi Network, Bandung.
-    </div>
-
-    <table class="table-signature">
-        <tr>
-            <td>Penanggung Jawab</td>
-            <td>Pelanggan</td>
-        </tr>
-        <tr>
-            <td class="space-sign"></td>
-            <td class="space-sign"></td>
-        </tr>
-        <tr>
-            <td class="sign-name">{{ strtoupper($personInCharge ?: 'IPIN ARIPIN') }}</td>
-            <td class="sign-name">{{ strtoupper($customerName) }}</td>
-        </tr>
-    </table>
 
 </body>
 </html>
